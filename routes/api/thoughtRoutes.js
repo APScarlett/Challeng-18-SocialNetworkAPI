40499b16518c5be56f0c8e4297eb443a0e407579
Alexander Scarlett <ascarlett92@yahoo.com>
2024-06-05 15:56:58 -0400

    updated thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,30 +1,30 @@
 const router = require('express').Router();
 const {
   getThoughts,
-  getSingleThought,
-  createThought,
-  updateThought,
-  deleteThought,
-  addThoughtResponse,
-  removeThoughtResponse,
-} = require('../../controllers/videoController');
+  getSingleThoughts,
+  createThoughts,
+  updateThoughts,
+  deleteThoughts,
+  addThoughtReaction,
+  removeThoughtReaction,
+} = require('../../controllers/thoughtController');
 
 // /api/thoughtss
 //http://localhost:3001/api/Thoughts
-router.route('/').get(getThoughts).post(createThought);
+router.route('/').get(getThoughts).post(createThoughts);
 
 // /api/videos/:videoId
 router
   .route('/:thoughtId')
-  .get(getSingleThought)
-  .put(updateThought)
-  .delete(deleteThought);
+  .get(getSingleThoughts)
+  .put(updateThoughts)
+  .delete(deleteThoughts);
 
 // /api/thought/:thoughtId/reactions
-//http://localhost:3001/api/Thoughts/
-router.route('/:thoughtId/reactions').post(addThoughtResponse);
+//http://localhost:3001/api/thoughts/
+router.route('/:thoughtId/reactions').post(addThoughtReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtId/reactions/:reactionId').delete(removeThoughtResponse);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeThoughtReaction);
 
 module.exports = router;
