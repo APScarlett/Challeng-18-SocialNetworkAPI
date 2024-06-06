dddc1a988e8821f272d49e0d7c9b22a534e76e87
Alexander Scarlett <ascarlett92@yahoo.com>
2024-06-06 08:34:31 -0400

    changed userRoutes back

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,16 +10,13 @@ const {
 } = require('../../controllers/userController');
 
 // /api/users
-
+//http://localhost:3001/api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// /api/users/:userId/friends
-router.route('/:userId/friends').post(addUserFriend);
-
-// /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').delete(removeUserFriend);
+//http://localhost:3001/api/users:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addUserFriend).delete(removeUserFriend);
 
 module.exports = router;
