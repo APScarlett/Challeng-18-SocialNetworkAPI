5f2c0c9c0f06996064ca8f31ecdf8ce6d8c0a155
Alexander Scarlett <ascarlett92@yahoo.com>
2024-06-05 20:35:31 -0400

    finished updating thoughtRoutes and began updating ThoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,23 +1,23 @@
-const { Video, User } = require('../models');
+const { Thought, User } = require('../models');
 
 module.exports = {
-  async getVideos(req, res) {
+  async getThoughts(req, res) {
     try {
-      const videos = await Video.find();
-      res.json(videos);
+      const thoughts = await Thought.find();
+      res.json(Thoughts);
     } catch (err) {
       res.status(500).json(err);
     }
   },
-  async getSingleVideo(req, res) {
+  async getThoughts(req, res) {
     try {
-      const video = await Video.findOne({ _id: req.params.videoId })
+      const thoughts = await Thought.findOne({ _id: req.params.videoId })
 
-      if (!video) {
-        return res.status(404).json({ message: 'No video with that ID' });
+      if (!thoughts) {
+        return res.status(404).json({ message: 'No thoughts with that ID' });
       }
 
-      res.json(video);
+      res.json(thoughts);
     } catch (err) {
       res.status(500).json(err);
     }
@@ -25,10 +25,10 @@ module.exports = {
   // create a new video
   async createVideo(req, res) {
     try {
-      const video = await Video.create(req.body);
+      const thoughts = await Thought.create(req.body);
       const user = await User.findOneAndUpdate(
         { _id: req.body.userId },
-        { $addToSet: { videos: video._id } },
+        { $addToSet: { thoughts: video._id } },
         { new: true }
       );
       //!undefined
