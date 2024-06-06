475a303aa686738360bbba61b208ba253c0909bd
Alexander Scarlett <ascarlett92@yahoo.com>
2024-06-06 09:20:32 -0400

    updated

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,14 +4,14 @@ module.exports = {
   async getThoughts(req, res) {
     try {
       const thoughts = await Thought.find();
-      res.json(Thoughts);
+      res.json(thoughts);
     } catch (err) {
       res.status(500).json(err);
     }
   },
-  async getSingleThoughts(req, res) {
+  async getSingleThought(req, res) {
     try {
-      const thought = await Thought.findOne({ _id: req.params.thoughtsId })
+      const thought = await Thought.findOne({ _id: req.params.thoughtId })
 
       if (!thought) {
         return res.status(404).json({ message: 'No thought with that ID' });
@@ -23,12 +23,12 @@ module.exports = {
     }
   },
 
-  // create a new Thoughts  //continue from here
-  async createThoughts(req, res) {
+  // create a new Thought  //continue from here
+  async createThought(req, res) {
     try {
       const thought = await Thought.create(req.body);
       const user = await User.findOneAndUpdate(
-        {username: req.body.username},
+        { _id: req.body.userId },
         { $addToSet: { thoughts: thought._id } },
         { new: true }
       );
@@ -45,10 +45,10 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  async updateThoughts(req, res) {
+  async updateThought(req, res) {
     try {
       const thought = await Thought.findOneAndUpdate(
-        { _id: req.params.thoughtsId },
+        { _id: req.params.thoughtId },
         { $set: req.body },
         { runValidators: true, new: true }
       );
@@ -63,9 +63,9 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  async deleteThoughts(req, res) {
+  async deleteThought(req, res) {
     try {
-      const thought = await Thought.findOneAndRemove({ _id: req.params.thoughtsId });
+      const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
 
       if (!thought) {
         return res.status(404).json({ message: 'No Thought with this id!' });
@@ -94,11 +94,11 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-  // Remove Thought response
+  // Remove Thought reaction
   async removeThoughtReaction(req, res) {
     try {
       const thought = await Thought.findOneAndUpdate(
-        { _id: req.params.thoughtsId },
+        { _id: req.params.thoughtId },
         { $pull: { reactions: { reactionId: req.params.reactionId } } },
         { runValidators: true, new: true }
       )
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,30 +1,30 @@
 const router = require('express').Router();
 const {
   getThoughts,
-  getSingleThoughts,
-  createThoughts,
-  updateThoughts,
-  deleteThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
   addThoughtReaction,
   removeThoughtReaction,
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
 //http://localhost:3001/api/Thoughts
-router.route('/').get(getThoughts).post(createThoughts);
+router.route('/').get(getThoughts).post(createThought);
 
 // /api/videos/:thoughtId
 router
   .route('/:thoughtsId')
-  .get(getSingleThoughts)
-  .put(updateThoughts)
-  .delete(deleteThoughts);
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
 // /api/thought/:thoughtId/reactions
 //http://localhost:3001/api/thoughts/
 router.route('/:thoughtId/reactions').post(addThoughtReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtsId/reactions/:reactionId').delete(removeThoughtReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeThoughtReaction);
 
 module.exports = router;
